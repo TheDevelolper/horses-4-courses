ddb4ec8bc711cd0353e08a11ecefbe5df0e682de
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2026-07-07 12:40:04 +0100

    clean: formatting fixes for horse component

--- projects/ui-components/src/lib/Molecules/Horse/horse.component.ts
+++ projects/ui-components/src/lib/Molecules/Horse/horse.component.ts
@@ -1,19 +1,13 @@
 import { Component, input } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 
 @Component({
-	selector: 'ui-horse',
-	templateUrl: './horse.component.html',
-	styleUrls: ['./horse.component.css'],
- imports: [CommonModule]
+  selector: 'ui-horse',
+  templateUrl: './horse.component.html',
+  styleUrls: ['./horse.component.css'],
+  imports: [CommonModule],
 })
 export class HorseComponent {
-	public number = input(1);
+  public number = input(1);
   public running = input(false);
-
-	constructor() { }
-
-	ngOnInit(): void {
-	}
-
 }
