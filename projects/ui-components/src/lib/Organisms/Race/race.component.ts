22eb5a9c16df2e6f3fcb129460ed6de1b3997c90
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2026-07-08 11:32:28 +0100

    chore: Scaffolded leaderboard component and story

--- projects/ui-components/src/lib/Organisms/Race/race.component.ts
+++ projects/ui-components/src/lib/Organisms/Race/race.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 
 import { HorseComponent } from '../../Molecules/Horse/horse.component';
+import { LeaderboardComponent } from '../../Molecules/Leaderboard/leaderboard.component';
 import { RacecourseComponent } from '../../Molecules/Racecourse/racecourse.component';
 import { RaceStore } from './race.store';
 
 @Component({
   selector: 'ui-race',
   standalone: true,
-  imports: [CommonModule, RacecourseComponent, HorseComponent],
+  imports: [CommonModule, RacecourseComponent, HorseComponent, LeaderboardComponent],
   templateUrl: './race.component.html',
   styleUrls: ['./race.component.css'],
 })
