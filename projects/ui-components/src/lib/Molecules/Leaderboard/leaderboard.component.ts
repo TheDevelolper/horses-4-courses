65aad93cc0fefc926a0448fd06e0b65b3edb8dd7
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2026-07-08 11:50:28 +0100

    feat: implement leaderboard component with dynamic data binding and styling

--- projects/ui-components/src/lib/Molecules/Leaderboard/leaderboard.component.ts
+++ projects/ui-components/src/lib/Molecules/Leaderboard/leaderboard.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RaceStore } from '../../Organisms/Race/race.store';
 
 @Component({
   selector: 'ui-leaderboard',
   imports: [],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LeaderboardComponent {}
+export class LeaderboardComponent {
+  public raceStore = inject(RaceStore);
+}
--- projects/ui-components/src/public-api.ts
+++ projects/ui-components/src/public-api.ts
@@ -3,6 +3,6 @@
  */
 
 export { HorseComponent } from './lib/Molecules/Horse/horse.component';
-
+export { LeaderboardComponent } from './lib/Molecules/Leaderboard/leaderboard.component';
 export { RaceComponent } from './lib/Organisms/Race/race.component';
 export { RaceStore } from './lib/Organisms/Race/race.store';
